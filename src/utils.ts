0b72298afb7619ce8ef77ced40dd4e9f100aee23
Alexis Tyler <xo@wvvw.me>
2020-10-02 23:07:04 +0930

    fix: remove uneeded dataloader

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,3 @@
-import DataLoader from 'dataloader';
-
-export const createLoader = (database: Map<string|number, any>) => new DataLoader(async (ids: any[]) => ids.map(id => database.get(id)));
 export const env = process.env['NODE_ENV'] ?? 'development';
 export const port = process.env['PORT'] ?? 0;
 export const debug = Boolean(process.env['DEBUG'] ?? false);
